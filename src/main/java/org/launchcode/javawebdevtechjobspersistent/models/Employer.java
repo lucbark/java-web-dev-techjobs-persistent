11b8362d9e87cd61e418a2c18434bc3d512f1a65
Cas <testing@owners-MacBook-Pro-2.local>
2020-10-28 21:05:26 -0500

    Need to finish Model section

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,31 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+
+    private String location;
+
+    @NotBlank(message = "Where is it?")
+    public Employer(String location){
+        this.location = location;
+    }
+
+
+
+    public Employer() {}
+
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 }
