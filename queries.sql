7dc119f80ad5735f0ed04f6aa74b590f93717728
Cas <testing@owners-MacBook-Pro-2.local>
2020-11-10 12:47:37 -0600

    Test it 1 and 2 are done

--- queries.sql
+++ queries.sql
@@ -1,7 +1,8 @@
 ## Part 1: Test it with SQL
-
+SHOW COLUMNS FROM job IN techjobs;
 ## Part 2: Test it with SQL
-
+SELECT name FROM techjobs.employer
+ WHERE location = 'St. Louis City ';
 ## Part 3: Test it with SQL
 
 ## Part 4: Test it with SQL
